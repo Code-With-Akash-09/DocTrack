a65ede77cc82d5920334fbc701d3253a962883bb
Code-With-Akash-09 <akashlakade130@gmail.com>
2026-07-15 22:07:08 +0530

    style: enhance NavigationPanel design & add hide-scrollbar utility

--- src/components/organism/navigationPanel.jsx
+++ src/components/organism/navigationPanel.jsx
@@ -7,23 +7,26 @@ import { Button } from "../ui/button";
 const NavigationPanel = ({ children }) => {
     return (
         <div className="flex min-h-0 w-full flex-1 flex-col md:rounded-md">
-            <div className="flex w-full shrink-0 h-16 bg-linear-to-r from-emerald-700 to-teal-800 md:rounded-t-md px-4 py-2">
-                <div className="flex h-full items-center w-full gap-1">
+            <div className="flex relative w-full shrink-0 h-16 md:rounded-t-md overflow-hidden">
+                <div className="flex w-full h-full absolute z-0 bg-linear-to-r from-green-300/30 via-teal-300/30 to-blue-400/30 backdrop-blur-sm" />
+                <div className="z-10 flex h-full items-center w-full gap-1 px-4 py-2 backdrop-blur-sm">
                     <Logo height={40} width={40} className={"size-10"} />
                     <span className="flex flex-col w-fit text-white">
-                        <span className="font-bold text-lg leading-none">
-                            Doctor Visit
+                        <span className="font-bold text-lg leading-none text-neutral-800">
+                            Doctor <span className="text-green-800">Visit</span>
                         </span>
-                        <span className="text-xs text-green-300"> Tracker</span>
+                        <span className="text-xs text-neutral-600">
+                            Tracker
+                        </span>
                     </span>
                 </div>
             </div>
-            <div className="min-h-0 flex-1 w-full bg-neutral-50 dark:bg-neutral-900 overflow-y-auto">
+            <div className="min-h-0 flex-1 w-full bg-neutral-50 dark:bg-neutral-900 overflow-y-auto hide-scrollbar">
                 {children}
             </div>
             <div
                 className={cn(
-                    "grid w-full shrink-0 h-16 bg-neutral-50 border-t border-neutral-200 md:rounded-b-md gap-4 px-4 py-2",
+                    "grid z-10 w-full shrink-0 bg-neutral-50 border-t border-neutral-200 h-16 md:rounded-b-md gap-4 px-4 py-2",
                     NAV_ITMES.length > 4
                         ? "grid-cols-4"
                         : `grid-cols-${NAV_ITMES.length}`,
@@ -38,9 +41,9 @@ const NavigationPanel = ({ children }) => {
                     >
                         <Link
                             href={item.href}
-                            className="flex flex-col items-center justify-center gap-1 text-sm font-bold text-muted-foreground hover:text-green-700 transition-colors duration-200"
+                            className="flex flex-col items-center justify-center gap-1 font-bold text-muted-foreground hover:text-green-700! transition-colors duration-200"
                         >
-                            <item.icon className="size-5!" />
+                            <item.icon className="size-4!" />
                             <span className="text-[10px]">{item.label}</span>
                         </Link>
                     </Button>
